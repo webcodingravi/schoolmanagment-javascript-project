3d1288a514a3694993b173ba4ead86f7c83ddc62
Ravi kumar <rkconsultancy34@gmail.com>
2025-06-03 20:57:04 +0530

    dashboard,payment section design

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,20 +1,42 @@
-  const ctx = document.getElementById('admission-chart');
+window.onload = () => {
+   admissionChart();
+   paymentChart();
+}
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+const admissionChart = () => {
+  const options = {
+     type: "bar",
+     data: {
+       labels: ['Jan','Feb','Mar','Apr','May','Jun'],
+       datasets: [{
+            label: 'Admissions',
+            data: [100,200,300,400,500,600],
+            backgroundColor: 'rgba(0,123,255,0.7)',
+            borderRadius:8
+       }
+             
+       ]
+     }
+  }
+  const canvas = document.getElementById('admission-chart').getContext('2d');
+  new Chart(canvas,options)
+}
+
+
+const paymentChart = () => {
+  const options = {
+     type: "doughnut",
+     data: {
+       labels: ['paid','Dues'],
+       datasets: [{
+            data: [35000,12000],
+            backgroundColor: ['#3eef5b','#fc4680'],
+            borderRadius:8
+       }
+             
+       ]
+     }
+  }
+  const canvas = document.getElementById('payment-chart').getContext('2d');
+  new Chart(canvas,options)
+}
